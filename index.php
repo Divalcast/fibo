7aeff732fc67a53f37fa19c7e9b5df991523169a
Divalcast <102643624+Divalcast@users.noreply.github.com>
2022-03-30 16:34:17 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $num1= 0;
+    $num2= 1;
+    $num3;
+    echo $num1 ."\n". $num2."\n";
+    for($i=0;$i<=20;$i++){
+        $num3= $num1 + $num2;
+        $num1= $num2;
+        $num2= $num3;
+        echo $num3 ."\n";
+    }
+?>
